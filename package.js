df889529612dc46feab984d7427f9d4f7553f182
Adithep <badsxx@gmail.com>
2014-08-09 12:45:53 +0700

    ma

--- package.js
+++ package.js
@@ -1,12 +1,20 @@
 Package.describe({
-  summary: "Server Functionality"
+  summary: "Server Functionality",
+  version: "0.0.1"
 });
 Package.on_use(function (api, where) {
-  api.use(['coffeescript', 'core-lib', 'accounts-base', 'accounts-password', 'mongo-aggregation']);
+  api.versionsFrom("METEOR-CORE@0.9.0-atm");
+  api.use([
+    'bads:core-lib',    
+    'bads:mongo-aggregation',
+    'accounts-base',
+    'accounts-password',
+    'coffeescript',
+  ]);
   api.add_files(['core-server.coffee', 'subscription.coffee'], 'server');
 });
 
 Package.on_test(function (api) {
-  api.use('core-server');
+  api.use("../packages/bads:core-server");
   api.add_files('core-server_tests.js', ['client', 'server']);
 });
